4263cd297653a16f13090c87e59b1adbab1bb2dc
Eddy <edward.gonzalez9708@gmail.com>
2019-02-16 08:54:12 -0500

    Completed Post and Put Endpoints.

--- src/main/java/com/lambdaschool/javadogs/DogsController.java
+++ src/main/java/com/lambdaschool/javadogs/DogsController.java
@@ -3,10 +3,11 @@ package com.lambdaschool.javadogs;
 import org.springframework.data.domain.Sort;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -73,4 +74,42 @@ public class DogsController {
 
         return new Resources<>(dogs, linkTo(methodOn(DogsController.class).isSuitable(suitable)).withSelfRel());
     }
+
+    @PutMapping("/dogs/{id}")
+    public ResponseEntity<?> replaceDog(
+            @RequestBody Dogs newDog,
+            @PathVariable Long id)
+            throws URISyntaxException {
+        Dogs updatedDog = dogsrepos
+                .findById(id)
+                .map(dog -> {
+                    dog.setBreed(newDog.getBreed());
+                    dog.setWeight(newDog.getWeight());
+                    dog.setSuitable(newDog.isSuitable());
+                    return dogsrepos.save(dog);
+                })
+                .orElseGet(() -> {
+                    newDog.setId(id);
+                    return dogsrepos.save(newDog);
+                });
+
+        Resource<Dogs> resource = assembler.toResource(updatedDog);
+
+        return ResponseEntity
+                .created(new URI(resource.getId().expand().getHref()))
+                .body(resource);
+    }
+
+    @PostMapping("/dogs")
+    public ResponseEntity<?> createDog(@RequestBody Dogs newDog)
+            throws URISyntaxException {
+        Dogs createdDog = dogsrepos.save(newDog);
+        Resource<Dogs> resource = assembler.toResource(createdDog);
+
+        return ResponseEntity
+                .created(new URI(resource.getId().expand().getHref()))
+                .body(resource);
+    }
+
+
 }
